fc9c0574311d1781076b14caf1511051f6b2312e
supermanas <manaskumar3003@gmail.com>
2023-10-09 23:38:55 +0530

    update

--- app/postarea/page.js
+++ app/postarea/page.js
@@ -10,7 +10,7 @@ export default function postarea(){
             <div class="lg:flex lg:items-center lg:justify-between">
   <div class="min-w-0 flex-1">
     <div className="pl-20">
-    <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">Back End Developer</h2></div></div></div>
+    <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">how come gdsc so good??</h2></div></div></div>
             </div>
             <div className="p-20">
                 <p className="text-xl">
